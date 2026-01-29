7276f956cee5f9ec2f04501ffeb4f0c2fbd9d152
Vishnu KR <vishnukr1995@gmail.com>
2026-01-29 15:25:41 +0530

    Disable S3 backend configuration in state.tf
    Comment out terraform backend configuration for S3.

--- envs/global/s3/state.tf
+++ envs/global/s3/state.tf
@@ -1,9 +1,9 @@
- terraform {
-   backend "s3" {
-     region       = "ap-south-1"
-     bucket       = "gitops-26-terraform-state"
-     key          = "global/s3/terraform.tfstate"
-     use_lockfile = true
-     encrypt      = true
-   }
- }
+# terraform {
+ #  backend "s3" {
+  #   region       = "ap-south-1"
+   #  bucket       = "gitops-26-terraform-state"
+    # key          = "global/s3/terraform.tfstate"
+     #use_lockfile = true
+     #encrypt      = true
+   #}
+ #}
